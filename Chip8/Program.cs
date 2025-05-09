0347ba6809af979797e262bbfeeed9ae2c8a16c9
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-05-09 00:37:31 -0300

    Doing SDL work

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SDL3;
+
+//Console.WriteLine("Current directory: " + Environment.CurrentDirectory);
+//Console.WriteLine("Looking for SDL3.dll...");
+
+
+SDL.SDL_Init(SDL.SDL_InitFlags.SDL_INIT_VIDEO);
+
+var window = SDL.SDL_CreateWindow("CHIP-8 interpreter", 64 * 8, 32 * 8, SDL.SDL_WindowFlags.SDL_WINDOW_ALWAYS_ON_TOP);
+
+
+Console.ReadLine();
