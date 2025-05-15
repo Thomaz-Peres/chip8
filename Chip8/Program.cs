a7339e68462dc6184edd2df7565cdb3c957e066e
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-05-14 23:49:48 -0300

    Doing some tests from docs

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -1,18 +1,35 @@
-﻿using SDL3;
+﻿using Sdl = SDL3.SDL;
 
-SDL.SDL_Init(SDL.SDL_InitFlags.SDL_INIT_VIDEO);
+bool done = false;
 
-var window = SDL.SDL_CreateWindow("CHIP-8 interpreter", 64 * 8, 32 * 8, SDL.
-SDL_WindowFlags.SDL_WINDOW_ALWAYS_ON_TOP);
+Sdl.SDL_Init(Sdl.SDL_InitFlags.SDL_INIT_VIDEO);
+
+var window = Sdl.SDL_CreateWindow("CHIP-8 interpreter", 640, 480, Sdl.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
 
 if (window == IntPtr.Zero)
 {
-    Console.WriteLine("SDL could not create a window.");
-    return;
+    // Sdl.SDL_LogCategory.SDL_LOG_CATEGORY_ERROR = 1;
+    Sdl.SDL_LogError(1, $"Could not create window: \n {Sdl.SDL_GetError()}");
+    Console.WriteLine("Sdl could not create a window.");
+    return 1;
 }
 
-var b = SDL.SDL_CreateRenderer(window, "teste");
+while (!done)
+{
+    Sdl.SDL_Event @event;
 
-Console.WriteLine(window);
+    while (Sdl.SDL_PollEvent(out @event))
+    {
+        if (@event.type == (uint)Sdl.SDL_EventType.SDL_EVENT_QUIT)
+            done = true;
+    }
+    var b = Sdl.SDL_CreateRenderer(window, x);
 
-Console.ReadLine();
+    // Sdl.
+}
+
+Sdl.SDL_DestroyWindow(window);
+
+Sdl.SDL_Quit();
+
+return 0;
