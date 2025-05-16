f592e3dc2fffaf5050855e0b967cbe7f3131f4d2
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-05-15 23:34:54 -0300

    Creating a red line, now I understand somethings

--- Chip8/CPU.cs
+++ Chip8/CPU.cs
@@ -3,5 +3,5 @@ namespace Chip8;
 public class CPU
 {
     private byte[] RAM = new byte[4096];
-    public static byte[] fonts = { 0xF0, };
+
 }
--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -2,34 +2,54 @@
 
 bool done = false;
 
-Sdl.SDL_Init(Sdl.SDL_InitFlags.SDL_INIT_VIDEO);
-
-var window = Sdl.SDL_CreateWindow("CHIP-8 interpreter", 640, 480, Sdl.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
-
-if (window == IntPtr.Zero)
+unsafe
 {
-    // Sdl.SDL_LogCategory.SDL_LOG_CATEGORY_ERROR = 1;
-    Sdl.SDL_LogError(1, $"Could not create window: \n {Sdl.SDL_GetError()}");
-    Console.WriteLine("Sdl could not create a window.");
-    return 1;
-}
+    Sdl.SDL_Init(Sdl.SDL_InitFlags.SDL_INIT_VIDEO);
 
-while (!done)
-{
-    Sdl.SDL_Event @event;
+    var window = Sdl.SDL_CreateWindow("CHIP-8 interpreter", 640, 640, Sdl.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
 
-    while (Sdl.SDL_PollEvent(out @event))
+    var renderer = Sdl.SDL_CreateRenderer(window, null);
+    Sdl.SDL_Texture texture = *Sdl.SDL_CreateTexture(renderer, Sdl.SDL_PixelFormat.SDL_PIXELFORMAT_RGBA8888, Sdl.SDL_TextureAccess.SDL_TEXTUREACCESS_TARGET, 640, 640);
+
+    if (window == IntPtr.Zero)
     {
-        if (@event.type == (uint)Sdl.SDL_EventType.SDL_EVENT_QUIT)
-            done = true;
+        // Sdl.SDL_LogCategory.SDL_LOG_CATEGORY_ERROR = 1;
+        Sdl.SDL_LogError(1, $"Could not create window: \n {Sdl.SDL_GetError()}");
+        Console.WriteLine("Sdl could not create a window.");
+        return 1;
     }
-    var b = Sdl.SDL_CreateRenderer(window, x);
 
-    // Sdl.
+    while (true)
+    {
+        Sdl.SDL_Event @event;
+        Sdl.SDL_PollEvent(out @event);
+
+        if (@event.type == (uint)Sdl.SDL_EventType.SDL_EVENT_QUIT)
+            break;
+
+
+        byte[] zero = { 0xF0, 0x90, 0x90, 0x90, 0xF0 };
+        Sdl.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        Sdl.SDL_RenderClear(renderer);
+        Sdl.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+        for (int i = 0; i < 640; i++)
+            Sdl.SDL_RenderPoint(renderer, i, i);
+
+        int x = 640;
+        for (int i = 0; i < 640; i++)
+        {
+            Sdl.SDL_RenderPoint(renderer, i, x);
+            x--;
+        }
+
+        Sdl.SDL_RenderPresent(renderer);
+    }
+
+    Sdl.SDL_DestroyRenderer(renderer);
+    Sdl.SDL_DestroyWindow(window);
+
+    Sdl.SDL_Quit();
+
+    return 0;
 }
-
-Sdl.SDL_DestroyWindow(window);
-
-Sdl.SDL_Quit();
-
-return 0;
