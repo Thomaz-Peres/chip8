72461ac299a3fc6e7f860b6dfa35bebe70c926d1
Thomaz Peres <thomaz.peres21@hotmail.com>
2025-05-17 00:48:55 -0300

    *

--- Chip8/CPU.cs
+++ Chip8/CPU.cs
@@ -3,5 +3,12 @@ namespace Chip8;
 public class CPU
 {
     private byte[] RAM = new byte[4096];
+    private Stack<ushort> Stack;
+    private readonly Timer DelayTimer;
+    private readonly byte[] V = new byte[16];
+    private readonly ushort PC = 0x200;
 
+    public CPU()
+    {
+    }
 }
--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -6,10 +6,10 @@ unsafe
 {
     Sdl.SDL_Init(Sdl.SDL_InitFlags.SDL_INIT_VIDEO);
 
-    var window = Sdl.SDL_CreateWindow("CHIP-8 interpreter", 640, 640, Sdl.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
+    var window = Sdl.SDL_CreateWindow("CHIP-8 interpreter", 640, 480, Sdl.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
 
     var renderer = Sdl.SDL_CreateRenderer(window, null);
-    Sdl.SDL_Texture texture = *Sdl.SDL_CreateTexture(renderer, Sdl.SDL_PixelFormat.SDL_PIXELFORMAT_RGBA8888, Sdl.SDL_TextureAccess.SDL_TEXTUREACCESS_TARGET, 640, 640);
+    Sdl.SDL_Texture texture = *Sdl.SDL_CreateTexture(renderer, Sdl.SDL_PixelFormat.SDL_PIXELFORMAT_RGBA8888, Sdl.SDL_TextureAccess.SDL_TEXTUREACCESS_TARGET, 640, 480);
 
     if (window == IntPtr.Zero)
     {
@@ -33,15 +33,19 @@ unsafe
         Sdl.SDL_RenderClear(renderer);
         Sdl.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
-        for (int i = 0; i < 640; i++)
-            Sdl.SDL_RenderPoint(renderer, i, i);
+        // for (int i = 0; i < 640; i++)
+        //     Sdl.SDL_RenderPoint(renderer, i, i);
 
-        int x = 640;
-        for (int i = 0; i < 640; i++)
+        foreach (var item in zero)
         {
-            Sdl.SDL_RenderPoint(renderer, i, x);
-            x--;
+            Sdl.SDL_RenderPoint(renderer, item, item);
         }
+        // int x = 640;
+        // for (int i = 0; i < 640; i++)
+        // {
+        //     Sdl.SDL_RenderPoint(renderer, i, x);
+        //     x--;
+        // }
 
         Sdl.SDL_RenderPresent(renderer);
     }
